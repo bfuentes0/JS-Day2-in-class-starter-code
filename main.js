c43cc429a5df49420a2ea9a3d1d72f6a9062cdd4
bfuentes0 <brandonfuentesc@utexas.edu>
2026-02-09 17:14:41 -0600

    First Commit

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 /////// Variables Section ///////
-const myName = "Clint Tuttle"
+const myName = "Brandon Fuentes-Coronado"
 const fruits = ['apples', 'bananas', 'blueberries', 'strawberries', 'pineapples'];
 const car = [{make:"Toyota", model:"Prius", color:"Silver"},
              {make:"Chevy", model:"Bolt", color:"Blue"}, 
@@ -33,12 +33,12 @@ function testLet(){
   console.log(a);
 }
 
-console.log(a);
-console.log(b);
-console.log("Example using var");
-testVar();
-console.log("Example using LET");
-testLet();
+//console.log(a);
+//console.log(b);
+//console.log("Example using var");
+//testVar();
+//console.log("Example using LET");
+//testLet();
 // When you are done with this section you can comment out the console.logs above
 
 
@@ -51,13 +51,13 @@ const result2Element = document.getElementById('result2');
 
 ////starter code////
 //update code below to using template literals (not concatenation) to pull in the variables myName, color, make, into output
-result2Element.innerHTML = "Hello my name is {yourname} and I drive a {White Honda Civic}";
+result2Element.innerHTML = `Hello, my name is ${myName}, and I drive a ${car[2].color} ${car[2].make} ${car[2].model}`;
 
 
 //Footer Section
 const footerElement = document.getElementById("footer");
 //update code below to use template literals to pull in the myName and the current year
-footerElement.innerHTML = "&copy; Clint Tuttle 2024";
+footerElement.innerHTML = `&copy; ${myName} ${today.getUTCFullYear()}`;
 
 
 
@@ -67,30 +67,25 @@ footerElement.innerHTML = "&copy; Clint Tuttle 2024";
 ////section 1 - basic if else statement
 if (loggedIn) {
     const result3Element = document.getElementById('result3');
-    result3Element.innerHTML = "Welcome back!";
+    function renderApp() { result3Element.innerHTML = "Welcome!";
+    }
 } else {
     const result3Element = document.getElementById('result3');
-    result3Element.innerHTML = "Please log in";
+    function renderLogin() { result3Element.innerHTML = "Please log in.";
+    }
 }
 
-////Sample code.  You can uncomment this to see how simple a ternaly operator can be
-// loggedIn ? alert("welcome back") : alert("please log in");
-
 ////section 2 -ternary operator to update the result3Element
-
+loggedIn ? renderApp() : renderLogin();
 
 
 
 
 //////// PART 4 - Arrow Functions////////
-function Greetings(name) {
-    return `Hello ${name}!`;
-};
 
-console.log(Greetings("Clint"));
-
-//Make a copy of the above function and rename it GreetingsArrow() and then convert function to an arrow function here
- 
+//GreetingsArrow() arrow function
+GreetingsArrow = (name) => `Hello ${name}!`;
+console.log(GreetingsArrow(myName));
 
 
 
@@ -101,20 +96,14 @@ function printFruit() {
 
     resultElement.textContent = "";
 
-    // Loop through the fruits array
-    for (let i = 0; i < fruits.length; i++) {
- 
-        // Create a list item for each fruit
-        let p = document.createElement("p");
-        p.textContent = fruits[i];
 
-        // Append the list item to the ordered list
-        resultElement.appendChild(p);
-    }
+    // As mentioned in the assignment, LLM was used to help create this function
+    fruits.filter(fruit => fruit.startsWith("b")).map(fruit => { let p = document.createElement("p"); 
+      p.textContent = fruit; 
+      resultElement.appendChild(p); });
 
  }
- 
- 
+
  function clearText() {
     let resultElement = document.getElementById("result5");
     resultElement.textContent = "";
@@ -122,26 +111,15 @@ function printFruit() {
 
 
 
-
-
  //////// PART6 - Destructuring////////
  myCar(car[0]);
 
- function myCar(car) {
-     var make = car.make;
-     var model = car.model;
-     var color = car.color;
- 
+ function myCar({color, make, model}) {
+     
      var message = `I drive a ${color} ${make} ${model}`;
    
      document.getElementById("result6").textContent = message;
  }
-
-//convert the myCar function to use destructuring
-
-
-
-
 
 
 
@@ -162,15 +140,14 @@ const baseProduct = {
   };
   
   // Merge basePrduct and Seasonal using spread operator and add lastUpdated property with a value of '2023-12-31'
-  ///UPDATE THIS CODE
-  const currentProduct = { 
-
+  const currentProduct = {...baseProduct, ...holidayPromo,
+    lastUpdated: `${today.getUTCFullYear()}-${today.getUTCMonth()}-${today.getUTCDate()}`
 };
   
   //Check the result object in the console:
   console.log(currentProduct);
   //UPDATE THIS OUTPUT
-  document.getElementById("result7").textContent = `The .....`; 
+  document.getElementById("result7").textContent = `The ${currentProduct.name} are on sale for ${currentProduct.name} ${currentProduct.price} as of ${currentProduct.lastUpdated}`; 
 
 
 
@@ -184,14 +161,19 @@ const baseProduct = {
     let x = document.getElementById("part8-input").value;
     
     try { 
-  
+    if(x.trim() == "") throw "EMPTY";
+    if(isNaN(x)) throw "NOT A NUMBER";
+    x = Number(x);
+    if(x > 15) throw "TOO HIGH";
+    if(x < 10) throw "TOO LOW";
+    if (x > 9 & x < 16) throw "Success";
     }
     catch(err) {
+      if (x > 9 & x < 16) message.innerHTML = "Success";
+      else message.innerHTML = "Input is " + err;
      } 
     finally {
-
     }
-
     
   }
 
